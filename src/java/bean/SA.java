a898f84c0bc3aa1f7fda2ba9fa993e3cceaaa4a0
Abel <Abel.ainou@gmail.com>
2013-08-14 13:08:08 +0100

    // hello c un test

---
+++ src/java/bean/SA.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import java.net.UnknownHostException;
+import java.util.Set;
+//hello world , this is a change 
+/**
+ *
+ * @author pc
+ */
+public class SA {
+   
+MongoClient mongoClient;
+ DB db;
+
+     public SA() throws UnknownHostException{
+     mongoClient = new MongoClient( "localhost" , 27017 );
+     db = mongoClient.getDB( "test" );
+     //Jongo jongo = new Jongo(db);
+     Set<String> colls = db.getCollectionNames();
+        for (String s : colls) {
+            System.out.println(s);
+        }
+     }
+}
